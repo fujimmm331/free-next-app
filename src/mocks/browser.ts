46fd1d84578d2230b805c3f0294002e20b7cea67
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2022-08-21 22:48:31 +0900

    feat:MSWの導入

---
+++ src/mocks/browser.ts
@@ -0,0 +1,7 @@
+/**
+ * ブラウザ用にAPIをモックするための設定ファイル
+ */
+import { setupWorker } from 'msw'
+import { handlers } from './handlers'
+// This configures a Service Worker with the given request handlers.
+export const worker = setupWorker(...handlers)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
+  const { worker } = require('../mocks/browser')
+  worker.start()
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
